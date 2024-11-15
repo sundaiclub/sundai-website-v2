0076359725ad13fe85c3c489051b53dc7e93f01f
Artem Lukoyanov <lukartoil@gmail.com>
2024-11-14 23:23:16 -0600

    UPD: fix fonts in the edit page

--- src/app/projects/[projectId]/edit/page.tsx
+++ src/app/projects/[projectId]/edit/page.tsx
@@ -20,7 +20,7 @@ function ButtonPanel({ params, router, isDarkMode, handleSave, saving }:
   { params: any, router: any, isDarkMode: boolean, 
     handleSave: () => void, saving: boolean }) {
   return (
-    <div className="flex items-center space-x-4 mt-4 text-sm">
+    <div className="flex items-center space-x-4 mt-4">
       <button
           onClick={() => router.push(`/projects/${params.projectId}`)}
           className={`flex items-center gap-2 px-4 py-2 transition-colors ${
@@ -301,7 +301,7 @@ export default function ProjectEditPage() {
               saving={saving} 
             />
           <div>
-            <label className={`block text-sm font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
+            <label className={`block font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
               Title
             </label>
             <input
@@ -311,19 +311,19 @@ export default function ProjectEditPage() {
               maxLength={MAX_TITLE_LENGTH}
               className={`mt-1 block w-3/4 border ${isDarkMode ? "border-gray-600 bg-gray-800 text-gray-100" : "border-gray-300 bg-white text-gray-900"} rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 p-2`}
             />
-            <span className={`text-sm ${isDarkMode ? "text-gray-400" : "text-gray-600"}`}>
+            <span className={` text-sm ${isDarkMode ? "text-gray-400" : "text-gray-600"}`}>
               {editableTitle.length}/{MAX_TITLE_LENGTH} characters
             </span>
           </div>
           <div>
-            <label className={`block text-sm font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
+            <label className={`block font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
               Tech Tags
             </label>
             <div className="flex flex-wrap gap-2 mt-2">
               {project?.techTags?.map((tag) => (
                 <span
                   key={tag.id}
-                  className={`px-2 py-1 rounded-full text-xs flex items-center gap-2 ${
+                  className={`px-2 py-1 rounded-full text-sm flex items-center gap-2 ${
                     isDarkMode
                       ? "bg-purple-900/50 text-purple-300"
                       : "bg-indigo-100 text-indigo-700"
@@ -338,7 +338,7 @@ export default function ProjectEditPage() {
               ))}
               <button
                 onClick={() => setShowTechTagModal(true)}
-                className={`px-2 py-1 rounded-full text-xs flex items-center ${
+                className={`px-2 py-1 rounded-full text-sm flex items-center ${
                   isDarkMode
                     ? "bg-purple-900/50 text-purple-300"
                     : "bg-indigo-100 text-indigo-700"
@@ -349,14 +349,14 @@ export default function ProjectEditPage() {
             </div>
           </div>
           <div>
-            <label className={`block text-sm font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
+            <label className={`block font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
               Domain Tags
             </label>
             <div className="flex flex-wrap gap-2 mt-2">
               {project?.domainTags?.map((tag) => (
                 <span
                   key={tag.id}
-                  className={`px-2 py-1 rounded-full text-xs flex items-center gap-2 ${
+                  className={`px-2 py-1 rounded-full text-sm flex items-center gap-2 ${
                     isDarkMode
                       ? "bg-gray-700 text-gray-300"
                       : "bg-gray-100 text-gray-700"
@@ -371,7 +371,7 @@ export default function ProjectEditPage() {
               ))}
               <button
                 onClick={() => setShowDomainTagModal(true)}
-                className={`px-2 py-1 rounded-full text-xs flex items-center ${
+                className={`px-2 py-1 rounded-full text-sm flex items-center ${
                   isDarkMode
                     ? "bg-gray-700 text-gray-300"
                     : "bg-gray-100 text-gray-700"
@@ -398,7 +398,7 @@ export default function ProjectEditPage() {
             type="domain"
           />
           <div>
-            <label className={`block text-sm font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
+            <label className={`block font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
               Project URLs
             </label>
             <form ref={formRef} onSubmit={(e) => {
@@ -429,7 +429,7 @@ export default function ProjectEditPage() {
             </form>
           </div>
           <div>
-            <label className={`block text-sm font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
+            <label className={`block font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
               One Sentence Description
             </label>
             <textarea
@@ -444,14 +444,14 @@ export default function ProjectEditPage() {
             </span>
           </div>
           <div>
-            <label className={`block text-sm font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
+            <label className={`block font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
               Start Date
             </label>
             <input
               type="date"
               value={editableStartDate ? format(editableStartDate, 'yyyy-MM-dd') : ''}
               onChange={(e) => setEditableStartDate(e.target.value ? new Date(e.target.value) : null)}
-              className={`mt-1 block w-64 border ${
+              className={`mt-1 block w-64 border${
                 isDarkMode 
                   ? "border-gray-600 bg-gray-800 text-gray-100 [color-scheme:dark] calendar-picker-indicator:filter-invert" 
                   : "border-gray-300 bg-white text-gray-900 [color-scheme:light]"
@@ -459,7 +459,7 @@ export default function ProjectEditPage() {
             />
           </div>
           <div>
-            <label className={`block text-sm font-medium mb-1 ${
+            <label className={`block font-medium mb-1 ${
               isDarkMode ? 'text-gray-300' : 'text-gray-700'
             } font-fira-code`}>
               Project Thumbnail
@@ -485,7 +485,7 @@ export default function ProjectEditPage() {
                   </button>
                 </div>
               )}
-              <label className={`cursor-pointer px-4 py-2 rounded-md shadow-sm text-sm font-medium ${
+              <label className={`cursor-pointer px-4 py-2 rounded-md shadow-sm font-medium ${
                 isDarkMode 
                   ? 'bg-gray-700 text-gray-300 hover:bg-gray-600' 
                   : 'bg-white text-gray-700 hover:bg-gray-50 border-2 border-gray-200 hover:border-gray-300'
@@ -506,9 +506,12 @@ export default function ProjectEditPage() {
             </p>
           </div>
           <div>
-            <label className={`block text-sm font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
+            <label className={`block font-medium ${isDarkMode ? "text-gray-300" : "text-gray-700"}`}>
               Full Description
             </label>
+            <span className={` text-sm ${isDarkMode ? "text-gray-400" : "text-gray-600"}`}>
+              Supports Markdown !
+            </span>
             <textarea
               value={editableDescription}
               onChange={(e) => setEditableDescription(e.target.value)}
