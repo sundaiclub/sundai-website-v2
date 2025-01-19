b4a92e7820b246f0ee55d8ad91a741d6f930d4e1
Connor Dirks <cdirks4@me.com>
2025-01-19 18:06:31 -0500

    tmp: breaks projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import ProjectGrid from "../components/Project";
+import SubmissionGrid from "../components/Submission";
 import { useTheme } from "../contexts/ThemeContext";
 
-export default function AllProjectsList() {
+export default function AllSubmissionsList() {
   const { isDarkMode } = useTheme();
 
   return (
@@ -13,9 +13,12 @@ export default function AllProjectsList() {
     >
       <div className={`max-w-7xl mx-auto px-2 sm:px-4 lg:px-8 py-20`}>
         <div className="flex flex-col space-y-4 mb-8">
+          <h1 className="text-3xl font-bold">
+            Full list of submissions in Sundai
+          </h1>
         </div>
 
-        <ProjectGrid showSearch={true}/>
+        <SubmissionGrid showSearch={true} />
       </div>
     </div>
   );
